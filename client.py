320426ecf2c967014e6051b9cc7fddce4b37f157
IthaloDekki <ithalomedei76@gmail.com>
2025-02-04 21:07:18 -0300

    ♻️ refactor: comentando código

--- client.py
+++ client.py
@@ -4,15 +4,17 @@ import ssl
 # Configurações do cliente
 HOST = '127.0.0.1'
 PORT = 4433
-SERVER_CERT = 'server.crt'
+SERVER_CERT = 'server.crt' # Certificado
 
-# Cria um socket TCP/IP
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# Cria um contexto SSL/TLS para o cliente
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT) #TLS
 context.load_verify_locations(SERVER_CERT)
 
+context.verify_mode(ssl.CERT_REQUIRED) # Exige a verificação do certificado
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+    with context.wrap_socket(sock, server_hostname=HOST) as ssock: # Envolve o socket com a camada SSL/TLS
         ssock.connect((HOST, PORT))
         ssock.sendall(b"GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
-        data = ssock.recv(1024)
+        data = ssock.recv(1024) # Recebe a resposta do servidor (até 1024 bytes)
         print(f"Recebido: {data.decode('utf-8')}")
--- service.py
+++ service.py
@@ -1,25 +1,25 @@
 import socket
 import ssl
 
-# Configurações do servidor
-HOST = '127.0.0.1'
+HOST = '127.0.0.1' # Endereço IP do servidor (localhost)
 PORT = 4433
-CERTIFICATE = 'server.crt'
-PRIVATE_KEY = 'server.key'
+CERTIFICATE = 'server.crt' # certificado
+PRIVATE_KEY = 'server.key'  # Chave Privada
 
 # Cria um socket TCP/IP
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile=CERTIFICATE, keyfile=PRIVATE_KEY)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) #TLS
+context.load_cert_chain(certfile=CERTIFICATE, keyfile=PRIVATE_KEY)  # Carrega o certificado e a chave privada
 
+# Cria um socket TCP/IP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    sock.bind((HOST, PORT))
+    sock.bind((HOST, PORT)) 
     sock.listen(5)
     print(f"Servidor escutando em {HOST}:{PORT}...")
 
-    with context.wrap_socket(sock, server_side=True) as ssock:
-        conn, addr = ssock.accept()
+    with context.wrap_socket(sock, server_side=True) as ssock: 
+        conn, addr = ssock.accept() # Conexão com cliente
         with conn:
             print(f"Conexão estabelecida com {addr}")
-            data = conn.recv(1024)
+            data = conn.recv(1024)  # Recebe dados do cliente (até 1024 bytes)
             print(f"Recebido: {data.decode('utf-8')}")
             conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, Client!")
